6e6ccae46e87a5de4f842f3562341e660ec64397
dustinggreenery <matthiasgschirmer@gmail.com>
2023-08-15 22:11:07 -0400

    connect button added

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,22 +1,20 @@
-import { Component } from 'react';
-import './header.css'
-import ContractButton from './contracts-button';
+import { Component } from "react";
+import "./header.css";
+import ContractButton from "./contracts-button";
+import { ConnectButton } from "web3uikit";
 
-class Header extends Component{
-    render(){
+class Header extends Component {
+    render() {
         return (
-        <div className="Header">
-            <header className="Header-header">
-                <p className="Header-title" >
-                Chicken Nuggets
-                </p>
-                <ContractButton />
-
-            </header>
-        </div>
+            <div className="Header">
+                <header className="Header-header">
+                    <p className="Header-title">Chicken Nuggets</p>
+                    <ConnectButton moralisAuth={false} />
+                    <ContractButton />
+                </header>
+            </div>
         );
     }
 }
-  
-  
-  export default Header;
+
+export default Header;
